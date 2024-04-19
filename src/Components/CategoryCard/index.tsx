18f29db0dac6c6f57366eecdb9fe65bb0ceeb3bb
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-19 10:18:24 -0300

    feat: add category card

---
+++ src/Components/CategoryCard/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+type Category = {
+  url: string;
+  alt: string;
+  name: string;
+};
+
+export default function CategoryCard({ url, alt, name }: Category) {
+  return (
+    <div className="category-card">
+      <img className="category-card-img" src={url} alt={alt} />
+      <h4 className="category-card-title">{name}</h4>
+    </div>
+  );
+}
