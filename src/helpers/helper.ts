840aeaea93933e19d94511e35f14e1d960732178
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-19 10:12:56 -0300

    refactor: add helpers

---
+++ src/helpers/helper.ts
@@ -0,0 +1,9 @@
+export const CONCURRENCY_TYPE = "Rp";
+export const CONCURRENCY_LOCALE = (function () {
+  return { type: "de-DE", options: {} };
+})();
+
+export const BRAND_NAME = "Furniro";
+
+export const ADDRESS = "400 University Drive Suite 200 Coral Gables,";
+export const LOCATION = "FL 33134 USA";
