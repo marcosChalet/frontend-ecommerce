b365df593a65793753f3982357f50193abf05d3f
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-19 10:15:48 -0300

    feat: add message in banner

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -3,10 +3,18 @@ import "./style.css";
 
 export default function Banner() {
   return (
-    <img
-      className="banner"
-      src={HomeBanner}
-      alt="Background banner with plants and a chair."
-    />
+    <div className="banner">
+      <img
+        className="banner-image"
+        src={HomeBanner}
+        alt="Background banner with plants and a chair."
+      />
+      <div className="banner-message">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis.
+        </p>
+      </div>
+    </div>
   );
 }
