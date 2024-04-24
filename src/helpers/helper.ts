cd66c2562b4e70982fc40e5f9e7a182f9cae86da
Marcos Chalet <chaletmarcos@gmail.com>
2024-04-24 10:57:04 -0300

    refactor: add links to social media

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -7,3 +7,7 @@ export const BRAND_NAME = "Furniro";
 
 export const ADDRESS = "400 University Drive Suite 200 Coral Gables,";
 export const LOCATION = "FL 33134 USA";
+
+export const FACEBOOK_PAGE_URL = "https://www.facebook.com";
+export const LINKEDIN_PAGE_URL = "https://www.linkedin.com/in/marcoschalet/";
+export const TWITTER_PAGE_URL = "https://twitter.com";
